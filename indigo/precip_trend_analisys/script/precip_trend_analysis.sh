682c51e09e7cfb759c8361b104ead85a602e4c49
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2016-07-24 16:24:11 +0200

    Copy output file to OPeNDAP and process NCL script in background mode

--- indigo/precip_trend_analisys/script/precip_trend_analysis.sh
+++ indigo/precip_trend_analisys/script/precip_trend_analysis.sh
@@ -20,10 +20,10 @@ fi
 
 # Publish output data using OPeNDAP
 mkdir -p $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID
-cp $InFile $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID/
+cp $InFile $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID/ 2>&1 > /dev/null &
 
 # Create and publish NCL map
-ncl "infile=\"$InFile\"" "outfile=\"$OutFile\"" $WorkDir/precip_trend_analysis.ncl &
+ncl "infile=\"$InFile\"" "outfile=\"$OutFile\"" $WorkDir/precip_trend_analysis.ncl 2>&1 > /dev/null &
 
 # Create and publish UV-CDAT map
 source activate ophidia-nox
