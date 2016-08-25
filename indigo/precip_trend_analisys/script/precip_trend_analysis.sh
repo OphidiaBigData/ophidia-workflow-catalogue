28a4e95ef8555c6f32b4282c41164ade7f86d450
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2016-08-25 17:10:53 +0200

    New version of INDIGO scripts

--- indigo/precip_trend_analisys/script/precip_trend_analysis.sh
+++ indigo/precip_trend_analisys/script/precip_trend_analysis.sh
@@ -1,24 +1,63 @@
 #!/bin/bash
 
 # Author: CMCC Foundation
-# Creation date: 02/11/2015
+# Creation date: 25/08/2016
 
 # This script assumes that UV-CDAT environment is
 UVCDATenv=ophidia-nox
 
 # This script assumes that BasePath to store output file for OPeNDAP is
-BasePath=/data/repository/INDIGO/precip_trend_input
+BasePath=$OPH_SCRIPT_DATA_PATH/INDIGO/precip_trend_input
 
 # Input parameters
-WorkDir=$1
-InFile=$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/$2
-NewGrid=$3
+FileName=${1}
+LatRange=${2}
+LonRange=${3}
+NewGrid=${4}
+
+RelWorkDir="`dirname \"$0\"`"
+AbsWorkDir="`( cd \"$RelWorkDir\" && pwd )`"
+InFile=$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/$FileName
 
 # Bilinear regridding
 if [ "$NewGrid" != "" ]; then
-	tmp=$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.tmp.nc
-	cdo remapbil,$NewGrid $InFile $tmp
-	mv $tmp $InFile
+
+XSIZE=${NewGrid%%x*}
+XSIZE=${XSIZE##*r}
+YSIZE=${NewGrid##*x}
+XFIRST=${LonRange%%:*}
+YFIRST=${LatRange%%:*}
+XLAST=${LonRange##*:}
+YLAST=${LatRange##*:}
+XINC=`echo "(($XLAST)-($XFIRST))/($XSIZE)" | bc -l`
+YINC=`echo "(($YLAST)-($YFIRST))/($YSIZE)" | bc -l`
+let XSIZE+=1
+let YSIZE+=1
+
+(
+cat <<'EOF'
+gridtype = lonlat
+xsize = XSIZE
+ysize = YSIZE
+xfirst = XFIRST
+xinc = XINC
+yfirst = YFIRST
+yinc = YINC
+EOF
+) > $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+sed -i "s/XSIZE/$XSIZE/g" $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+sed -i "s/YSIZE/$YSIZE/g" $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+sed -i "s/XFIRST/$XFIRST/g" $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+sed -i "s/YFIRST/$YFIRST/g" $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+sed -i "s/XINC/$XINC/g" $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+sed -i "s/YINC/$YINC/g" $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+
+tmp=$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.tmp.nc
+cdo remapbil,$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid $InFile $tmp
+mv $tmp $InFile
+
+rm -f $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/.grid
+
 fi
 
 # Publish output data using OPeNDAP
@@ -27,7 +66,7 @@ cp $InFile $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID/ 2>&1 > /d
 
 # Create and publish UV-CDAT map
 source activate $UVCDATenv
-python $WorkDir/precip_trend_analysis.py $WorkDir $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/
+python $AbsWorkDir/precip_trend_analysis.py $AbsWorkDir $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/
 source deactivate
 
 exit 0
--- indigo/precip_trend_analisys/script/precip_trend_analysis_ensemble.sh
+++ indigo/precip_trend_analisys/script/precip_trend_analysis_ensemble.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Author: CMCC Foundation
-# Creation date: 02/11/2015
+# Creation date: 25/08/2016
 
 # This script assumes that UV-CDAT environment is
 UVCDATenv=ophidia-nox
 
 # This script assumes that BasePath to store output file for OPeNDAP is
-BasePath=/data/repository/INDIGO/precip_trend_output
+BasePath=$OPH_SCRIPT_DATA_PATH/INDIGO/precip_trend_output
 
 # Input parameters
-WorkDir=$1
-InFile=$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/${2}.nc
+FileNameWithoutExtension=${1}
+
+RelWorkDir="`dirname \"$0\"`"
+AbsWorkDir="`( cd \"$RelWorkDir\" && pwd )`"
+InFile=$OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/$FileNameWithoutExtension.nc
 
 # Publish output data using OPeNDAP
 mkdir -p $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID
@@ -19,7 +22,7 @@ cp $InFile $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID/ 2>&1 > /d
 
 # Create and publish UV-CDAT map
 source activate $UVCDATenv
-python $WorkDir/precip_trend_analysis_ensemble.py $WorkDir $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/ $2
+python $AbsWorkDir/precip_trend_analysis_ensemble.py $AbsWorkDir $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/ $FileNameWithoutExtension
 source deactivate
 
 exit 0
