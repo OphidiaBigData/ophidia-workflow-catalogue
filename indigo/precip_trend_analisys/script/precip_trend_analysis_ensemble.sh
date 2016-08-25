d6dd24029fd2268fe63009c04c6bded4170e4055
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2016-08-25 15:25:39 +0200

    Add some comments to INDIGO scripts

--- indigo/precip_trend_analisys/script/precip_trend_analysis_ensemble.sh
+++ indigo/precip_trend_analisys/script/precip_trend_analysis_ensemble.sh
@@ -3,9 +3,10 @@
 # Author: CMCC Foundation
 # Creation date: 02/11/2015
 
-# This script assumes that UV-CDAT environment is called 'ophidia-nox'
-# This script assumes that BasePath to store output file for OPeNDAP is
+# This script assumes that UV-CDAT environment is
+UVCDATenv=ophidia-nox
 
+# This script assumes that BasePath to store output file for OPeNDAP is
 BasePath=/data/repository/INDIGO/precip_trend_output
 
 # Input parameters
@@ -17,7 +18,7 @@ mkdir -p $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID
 cp $InFile $BasePath/$OPH_SCRIPT_SESSION_CODE/$OPH_SCRIPT_WORKFLOW_ID/ 2>&1 > /dev/null &
 
 # Create and publish UV-CDAT map
-source activate ophidia-nox
+source activate $UVCDATenv
 python $WorkDir/precip_trend_analysis_ensemble.py $WorkDir $OPH_SCRIPT_SESSION_PATH/$OPH_SCRIPT_WORKFLOW_ID/ $2
 source deactivate
 
